52b48e963c27f6f11f5957044c5264e9b5f3df93
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2023-10-23 13:29:59 +0530

    First commit

---
+++ ZSGS/src/Xpattern.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Xpattern {
+	public static void main(String[] args) {
+      String str=getString();
+        if(str.length()%2==0)
+           isEven();
+         else
+           isOdd(str);
+	}
+	private static String getString() {
+		Scanner scan=new Scanner(System.in);
+		 System.out.print("Enter the String : ");
+		  String str=scan.nextLine();
+		   scan.close();
+		   return str;
+		   
+	}
+	private static void isEven() {
+		System.out.println(0);
+		
+	}
+	
+	private static void isOdd(String str) {
+		int n=str.length();
+		for(int i=0;i<n;i++) {
+		   for(int j=0;j<n;j++) {
+			    if(i==j)
+					System.out.print(str.charAt(i)+" ");
+				else if(i+j==(n-1))
+					System.out.print(str.charAt(j)+" ");
+				else
+					System.out.print("  ");
+		}
+		            System.out.println();
+	}
+
+}
+}
